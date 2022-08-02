cdcf57e5e6d0307c88eab93403c9622d87d8065d
Swayam Prakash <107098740+swayampandey@users.noreply.github.com>
2022-08-02 20:14:22 +0530

    Update EvenOdd.java

--- src/com/bridgelabz/assignment/EvenOdd.java
+++ src/com/bridgelabz/assignment/EvenOdd.java
@@ -7,14 +7,21 @@ public class EvenOdd {
 	static int EvenSum;
 	static int OddSum;
 	public static void main(String[] args) {
+		int i,EvenSum=0,OddSum=0;
 		for(i=1;i<=20;i++) {
 		
-			if(i%2 ==0 && j%2!=0)
-				System.out.println(i+"\t"+j);
-				j++;
-			
+			if(i%2!=0 ) {
+				System.out.print(i);
+				OddSum=OddSum+i;
+			}
+			else {
+				System.out.println("\t"+i);
+				EvenSum=EvenSum+i;
+			}
 		}
-	//	System.out.println(EvenSum+"\t"+OddSum);
+		System.out.println("--------------");
+		System.out.println("Total\tTotal");
+		System.out.println(OddSum+"\t"+EvenSum);
 			
 				
 		
